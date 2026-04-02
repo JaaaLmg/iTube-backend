c57f95197364663c905216ee5dd70d3f66235fd1
LumingJia <1752482617@qq.com>
2026-04-02 08:42:03 +0800

    完善启动类配置

--- iTube-admin/src/main/java/com/ja/itubeadmin/ITubeAdminApplication.java
+++ iTube-admin/src/main/java/com/ja/itubeadmin/ITubeAdminApplication.java
@@ -1,10 +1,16 @@
 package com.ja.itubeadmin;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@SpringBootApplication(scanBasePackages = {"com.ja"}, exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication(scanBasePackages = {"com.ja"})
+@MapperScan("com.ja.itubecommon.mappers")
+@EnableTransactionManagement
+@EnableScheduling
 public class ITubeAdminApplication {
 
     public static void main(String[] args) {
